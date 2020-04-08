da3f030ef89028f9226efaf29c50b07335c1d0f9
issnow <issnow.guo@gmail.com>
2020-04-08 23:38:45 +0800

    feat:日常更新

--- js/2.js
+++ js/2.js
@@ -1,3 +1,5 @@
+// 多位数字累乘到1位数的次数   123 => 1
+
 // persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
 // and 4 has only one digit
 
@@ -5,7 +7,35 @@
  // 1*2*6 = 12, and finally 1*2 = 2
 
 // persistence(4) === 0 // because 4 is already a one-digit number
-// 次数
-function persistence(n) {
+// function persistence(n) {
+//   let arr = n.toString().split('')
+//   let count = 0
+//   while(arr.length> 1) {
+//     arr = arr.reduce((p,n)=>p*n).toString().split('')
+//     count++
+//   }
+//   return count
+// }
+// persistence(999)
+console.log(persistence(999));
+console.log(persistence(4));
+console.log(persistence(23));
+
+// function persistence(num) {
+//   var times = 0;
   
+//   num = num.toString();
+  
+//   while (num.length > 1) {
+//     times++;
+//     num = num.split('').map(Number).reduce((a, b) => a * b).toString();
+//   }
+  
+//   return times;
+// }
+
+function persistence(n) {
+  return `${n}`.length > 1
+  ? 1+persistence(`${n}`.split('').reduce((p,n)=>p*n)) 
+  : 0
 }
